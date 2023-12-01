c9162399bbe05df7fbc96d24dc559e3c24fc2110
SemKirkels <Sem.kirkels@hotmail.com>
2023-12-01 13:36:16 +0100

    Added Day 1

---
+++ day_1/day_1_part_1.py
@@ -0,0 +1,16 @@
+fd = open("input.txt")
+
+count = 0
+
+for line in fd:
+    for i in line:
+        if i.isdigit():
+            count += int(i) * 10
+            break
+            
+    for i in reversed(line):
+        if i.isdigit():
+            count += int(i)
+            break
+
+print(count)
